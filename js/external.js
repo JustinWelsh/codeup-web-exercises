4ec51c0f6c34aa27a64ac51dcbba68026bc48bdc
Justin Welsh <justinwelsh33@gmail.com>
2022-04-14 00:03:55 -0500

    Finished last 2 problems

--- js/external.js
+++ js/external.js
@@ -1,31 +1,33 @@
-// console.log("Hello from external JavaScript");
-//
-// alert('Welcome to my Website!')
-// let userFavColor = prompt("Whats your favorite color?")
-// alert(userFavColor + " is my favorite color too!")
+console.log("Hello from external JavaScript");
+
+alert('Welcome to my Website!')
+let userFavColor = prompt("Whats your favorite color?")
+alert(userFavColor + " is my favorite color too!")
+
+
 //--------------------------------------------------------
 
 
-// const rentalCost = 3
-//
-// let lMermaidRentalDays = parseFloat( prompt( "How long would you like to rent The Little Mermaid?"))
-// alert("You are renting The Little Mermaid for " + lMermaidRentalDays + " days.")
-//
-// alert("Your total is " + "$" + lMermaidRentalDays * rentalCost)
-//
-// let bBearRentalDays = parseFloat( prompt( "How long would you like to rent Brother Bear?"))
-// alert("You are renting Brother Bear for " + bBearRentalDays + " days.")
-//
-// alert("Your total is " + "$" + bBearRentalDays * rentalCost)
-//
-// let herculesRentalDays = parseFloat( prompt( "How long would you like to rent Hercules?"))
-// alert("You are renting Hercules for " + herculesRentalDays + " days.")
-//
-// alert("Your total is " + "$" + herculesRentalDays * rentalCost)
-//
-// let rentalTotalCost = (lMermaidRentalDays + bBearRentalDays + herculesRentalDays) * rentalCost
-//
-// alert("Your grand total is $" + rentalTotalCost)
+const rentalCost = 3
+
+let lMermaidRentalDays = parseFloat( prompt( "How long would you like to rent The Little Mermaid?"))
+alert("You are renting The Little Mermaid for " + lMermaidRentalDays + " days.")
+
+alert("Your total is " + "$" + lMermaidRentalDays * rentalCost)
+
+let bBearRentalDays = parseFloat( prompt( "How long would you like to rent Brother Bear?"))
+alert("You are renting Brother Bear for " + bBearRentalDays + " days.")
+
+alert("Your total is " + "$" + bBearRentalDays * rentalCost)
+
+let herculesRentalDays = parseFloat( prompt( "How long would you like to rent Hercules?"))
+alert("You are renting Hercules for " + herculesRentalDays + " days.")
+
+alert("Your total is " + "$" + herculesRentalDays * rentalCost)
+
+let rentalTotalCost = (lMermaidRentalDays + bBearRentalDays + herculesRentalDays) * rentalCost
+
+alert("Your grand total is $" + rentalTotalCost)
 //--------------------------------------------------------
 
 
@@ -44,6 +46,33 @@ alert("Your total pay for the week is $" + totalPay)
 
 
 //--------------------------------------------------------
+
+
+let isClassFull = false;
+let doesConflictWithSchedule = true;
+let canEnroll = !isClassFull && !doesConflictWithSchedule;
+canEnroll && alert("You can enroll")
+!canEnroll && alert("You can't enroll")
+
+
+//--------------------------------------------------------
+
+
+let hasOfferExpired = false;
+let hasMoreThanTwoItems = true;
+let isPremiumMember = true;
+
+let canGetOffer = !hasOfferExpired && (hasMoreThanTwoItems || isPremiumMember)
+console.log(canGetOffer);
+
+canGetOffer && alert("Congrats!!")
+!canGetOffer && alert("Sorry")
+
+
+
+
+
+
 
 
 
